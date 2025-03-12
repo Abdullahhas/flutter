6449d77338d1329e4c9af612402ffdc66c3c5d61
abdullahhas <abdullah312003@gmail.com>
2025-03-12 19:59:56 +0500

    row col

--- newroject/lib/main.dart
+++ newroject/lib/main.dart
@@ -3,31 +3,57 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Home(),
+      home: NinCard(),
     ));
 }
 
-
-
-class Home extends StatelessWidget{
+class NinCard extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('My First Apps'),
-          centerTitle: true,
-          backgroundColor: Colors.blue[400],
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('My ID Card',
+        style : TextStyle(
+          color: Colors.white,
+        )
         ),
-        body: Center(
-          child: Image.asset('assets/p1.jpg'),
-          
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click me'),
-          backgroundColor: Colors.red[400],
-        ),
-      );
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+       
+        
+      ),
+    );
   }
 }
+
+
+
+
+
+// class Home extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context)
+//   {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text('My First Apps'),
+//           centerTitle: true,
+//           backgroundColor: Colors.blue[400],
+//         ),
+//         body:Padding(
+//           padding: EdgeInsets.all(20.0),
+//           child: Text('Hello World'),
+//         ), 
+          
+        
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {},
+//           child: Text('Click me'),
+//           backgroundColor: Colors.red[400],
+//         ),
+//       );
+//   }
+// }
+
