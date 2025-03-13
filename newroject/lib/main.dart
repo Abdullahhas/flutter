a884d1ac62f0931c1d5d58f7373649da9f2f984c
abdullahhas <abdullah312003@gmail.com>
2025-03-13 23:22:06 +0500

    understanding the scaffold etc...

--- newroject/lib/main.dart
+++ newroject/lib/main.dart
@@ -1,59 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: NinCard(),
-    ));
+  runApp(MyApp());
 }
 
-class NinCard extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('My ID Card',
-        style : TextStyle(
-          color: Colors.white,
+    return MaterialApp(
+      home: Scaffold(
+       body:  Center(
+        child: Text("Hello!!!"),
         )
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-       
-        
-      ),
-    );
+        )
+        );
   }
 }
-
-
-
-
-
-// class Home extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context)
-//   {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text('My First Apps'),
-//           centerTitle: true,
-//           backgroundColor: Colors.blue[400],
-//         ),
-//         body:Padding(
-//           padding: EdgeInsets.all(20.0),
-//           child: Text('Hello World'),
-//         ), 
-          
-        
-//         floatingActionButton: FloatingActionButton(
-//           onPressed: () {},
-//           child: Text('Click me'),
-//           backgroundColor: Colors.red[400],
-//         ),
-//       );
-//   }
-// }
-
