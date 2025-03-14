660568436d76811b3b393951a3eeacb07ea10ce3
abdullahhas <abdullah312003@gmail.com>
2025-03-14 22:30:12 +0500

    curreny calculator

--- newroject/lib/main.dart
+++ newroject/lib/main.dart
@@ -10,32 +10,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
-       body: Center(
-         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('0' , style: TextStyle(
-              fontSize: 45,
-              fontWeight: FontWeight.bold
-            ),
-            ),
-            TextField(
-              
-              style: TextStyle(
-
-                color: Colors.white
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '0',
+                style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
               ),
-              decoration: InputDecoration(
-               hintText: "Plz enter the amount in USD",
-               hintStyle: TextStyle(
-                color: Colors.white60,
-               )
+              TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "Plz enter the amount in USD",
+                  hintStyle: TextStyle(color: Colors.white60),
+                ),
               ),
-            ),
-          ],
-         ),
-       ) 
+            ],
+          ),
         ),
-        );
+      ),
+    );
   }
 }
