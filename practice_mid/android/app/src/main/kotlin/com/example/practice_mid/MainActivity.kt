75d9aa2ad134857b96dca9f33c8be9d7c39e9b69
abdullahhas <abdullah312003@gmail.com>
2025-04-03 15:28:46 +0500

    prac mid

---
+++ practice_mid/android/app/src/main/kotlin/com/example/practice_mid/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.practice_mid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ practice_mid/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Flutter Container'
+            ),
+        ),
+        body:  Center(
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+            child : Center(
+              child: Text(
+                'Hello World',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        )
+      ),
+     
+    );
+    
+  }
+}
